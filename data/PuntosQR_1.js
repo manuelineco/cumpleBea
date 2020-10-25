627c3cfc4217e1ce19207525a111274cd7068686
manuelineco <59016245+manuelineco@users.noreply.github.com>
2020-10-26 00:04:59 +0100

    Add files via upload

---
+++ data/PuntosQR_1.js
@@ -0,0 +1,17 @@
+var json_PuntosQR_1 = {
+"type": "FeatureCollection",
+"name": "PuntosQR_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Sitio": "Casita", "Pista": "A buscar, por desgracia no soy un sobao, me encontrarías demasiado rápido", "Parada": 1.0 }, "geometry": { "type": "Point", "coordinates": [ -3.669364893544056, 40.427124121879828 ] } },
+{ "type": "Feature", "properties": { "Sitio": "Gim", "Pista": "La respuesta está en tus compañeros", "Parada": 2.0 }, "geometry": { "type": "Point", "coordinates": [ -3.665472452321629, 40.427181691573907 ] } },
+{ "type": "Feature", "properties": { "Sitio": "Pista", "Pista": "Estoy cabezaabajo", "Parada": 3.0 }, "geometry": { "type": "Point", "coordinates": [ -3.665260017638466, 40.427012180230427 ] } },
+{ "type": "Feature", "properties": { "Sitio": "Fuente del Berro", "Pista": "La esquina, estoy en la esquina", "Parada": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -3.66090216066803, 40.428898293730491 ] } },
+{ "type": "Feature", "properties": { "Sitio": "Parque", "Pista": "Piensa en verde", "Parada": 5.0 }, "geometry": { "type": "Point", "coordinates": [ -3.669080238644844, 40.429968013065519 ] } },
+{ "type": "Feature", "properties": { "Sitio": "Wizink", "Pista": "Estoy en el mobiliario", "Parada": 6.0 }, "geometry": { "type": "Point", "coordinates": [ -3.672582091218774, 40.423546658120614 ] } },
+{ "type": "Feature", "properties": { "Sitio": "Pepita", "Pista": "Parece un buen sitio para pararse a hablar y resguardarse de la lluvia", "Parada": 9.0 }, "geometry": { "type": "Point", "coordinates": [ -3.668698480138026, 40.427461563935886 ] } },
+{ "type": "Feature", "properties": { "Sitio": "Parada Manuel Becerra", "Pista": "Búscame por las barandillas", "Parada": 10.0 }, "geometry": { "type": "Point", "coordinates": [ -3.669275164820553, 40.427613642365614 ] } },
+{ "type": "Feature", "properties": { "Sitio": "Puente", "Pista": "Me encanta estar en medio", "Parada": 8.0 }, "geometry": { "type": "Point", "coordinates": [ -3.668471556314502, 40.42682221131426 ] } },
+{ "type": "Feature", "properties": { "Sitio": "Maca", "Pista": "Contra la pared", "Parada": 7.0 }, "geometry": { "type": "Point", "coordinates": [ -3.664189360233959, 40.425170719368175 ] } }
+]
+}
